111d1f9dbb4f03f15890dd515f6853101a3630ea
billyninja <joaoeduardocm@gmail.com>
2016-02-24 21:49:08 -0500

    changing some settings for production environ compatibility

--- core/settings.py
+++ core/settings.py
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/puzzler/static'
+STATICFILES_DIRS = ['/home/puzzler/static']
